4abf5ed64e6984b97a9923122eb9aebf3fcc8d49
tuananhwtpq <tuananhwtpq@gmail.com>
2025-12-03 16:39:36 +0700

    feat: trans, done all feat

--- app/src/androidTest/java/com/example/baseproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/baseproject/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.example.baseproject
+package com.flowart.ar.drawing.sketch
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.baseproject", appContext.packageName)
+        assertEquals("com.flowart.ar.drawing.sketch", appContext.packageName)
     }
 }
